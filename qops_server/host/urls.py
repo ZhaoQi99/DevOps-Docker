943d8ce6e9ce4a481473c69aea8c26a0eeafaaa1
Qi Zhao <zhaoqi99@outlook.com>
2020-03-15 21:57:36 +0800

    test: 💍 fix login testcase

--- qops_server/host/urls.py
+++ qops_server/host/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HostView, web_ssh
 
 urlpatterns = [
-    path('', HostView.as_view(), name='user_login'),
+    path('', HostView.as_view(), name='hosts'),
     path('ssh/<int:h_id>/', web_ssh),
 ]
