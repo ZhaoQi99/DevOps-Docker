f80033799a0310f847db106e1db7cd97e3a3193f
Qi Zhao <zhaoqi99@outlook.com>
2020-03-15 21:29:44 +0800

    fix: 🐛 requirements file

--- qops_server/qops_server/settings/dev.py
+++ qops_server/qops_server/settings/dev.py
@@ -16,22 +16,3 @@ DATABASES = {
 
 AUTH_CONFIG['AUTH_TOKEN_EXPIRE'] = timedelta(days=10)
 MIDDLEWARE.pop(5)  # CSRF
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://139.9.236.103:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("139.9.236.103", 6379)],
-        },
-    },
-}
