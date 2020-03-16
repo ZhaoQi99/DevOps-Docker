f4584822af19a06929534477f12b6aebc6c6ca7d
Qi Zhao <zhaoqi99@outlook.com>
2020-03-16 21:34:11 +0800

    fix: 🐛 ws routing

--- qops_web/vue.config.js
+++ qops_web/vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8000"
+      },
+      "/ws": {
+        target: "http://127.0.0.1:8000"
       }
     },
     disableHostCheck: true
